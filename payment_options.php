99975de37ed3fd21b8f51d6788d3a80653097078
Protyay Roy <98158272+Protyay-Roy@users.noreply.github.com>
2022-08-10 22:18:36 +0600

    Add files via upload
    payment page

---
+++ payment_options.php
@@ -0,0 +1,45 @@
+<?php include "includes/header.php" ?>
+
+<div class="box"><!-- box Begin -->
+    
+    <h1 class="text-center">Payment Options For You</h1>  
+    
+     <p class="lead text-center"><!-- lead text-center Begin -->
+         
+         <a class="" href="#"> Offline Payment </a>
+         
+     </p><!-- lead text-center Finish -->
+     
+     <center><!-- center Begin -->
+         
+        <p class="lead"><!-- lead Begin -->
+            
+            <a href="#">
+                
+                Paypall Payment
+                
+                <img class="img-responsive" src="images/paypall_img.png" alt="img-paypall"
+                
+                style="height: 300px;padding-top: 30px;">
+                
+            </a>
+            
+        </p> <!-- lead Finish -->
+         
+     </center><!-- center Finish -->
+    
+</div><!-- box Finish -->
+
+<?php
+
+include("includes/footer.php");
+
+?>
+
+<script src="js/jquery-331.min.js"></script>
+<script src="js/bootstrap-337.min.js"></script>
+
+
+</body>
+
+</html>
