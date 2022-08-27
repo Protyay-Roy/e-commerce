3f289cc6601a8db8c3de0a75eebcd557dfd0b145
Protyay Roy <98158272+Protyay-Roy@users.noreply.github.com>
2022-08-27 23:47:55 +0600

    Add files via upload

---
+++ admin_area/view_payments.php
@@ -0,0 +1,114 @@
+<?php
+
+if (!isset($_SESSION['admin_email'])) {
+
+    echo "<script>window.open('login.php','_self')</script>";
+} else {
+
+?>
+
+    <div class="row">
+        <!-- row 1 begin -->
+        <div class="col-lg-12">
+            <!-- col-lg-12 begin -->
+            <ol class="breadcrumb">
+                <!-- breadcrumb begin -->
+                <li class="active">
+                    <!-- active begin -->
+
+                    <i class="fa fa-dashboard"></i> Dashboard / View Payments
+
+                </li><!-- active finish -->
+            </ol><!-- breadcrumb finish -->
+        </div><!-- col-lg-12 finish -->
+    </div><!-- row 1 finish -->
+
+    <div class="row">
+        <!-- row 2 begin -->
+        <div class="col-lg-12">
+            <!-- col-lg-12 begin -->
+            <div class="panel panel-default">
+                <!-- panel panel-default begin -->
+                <div class="panel-heading">
+                    <!-- panel-heading begin -->
+                    <h3 class="panel-title">
+                        <!-- panel-title begin -->
+
+                        <i class="fa fa-fw fa-money"></i> View Payments
+
+                    </h3><!-- panel-title finish -->
+                </div><!-- panel-heading finish -->
+
+
+                <div class="panel-body">
+                    <!-- panel-body begin -->
+                    <div class="table-responsive">
+                        <!-- table-responsive begin -->
+                        <table class="table table-hover table-striped table-bordered">
+                            <!-- table table-hover table-striped table-bordered begin -->
+
+                            <thead>
+                                <!-- thead begin -->
+
+                                <tr>
+                                    <!-- th begin -->
+
+                                    <th> Order no: </th>
+                                    <th> Invoice No: </th>
+                                    <th> Amount: </th>
+                                    <th> Payment Mode: </th>
+                                    <th> Ref No: </th>
+                                    <th> Code: </th>
+                                    <th> Payment Date: </th>
+
+                                </tr><!-- th finish -->
+
+                            </thead><!-- thead finish -->
+
+                            <tbody>
+                                <!-- tbody begin -->
+
+                            <?php
+                            
+                                $s = "SELECT * FROM `payments`";
+                                if ($q = $con->query($s)) {
+
+                                    $i = 0;
+                                    while ($r = $q->fetch_assoc()) {  $i++;?>
+                                    
+                                <tr>
+                                    <!-- tr begin -->
+                                    <td><?= $i; ?></td>
+                                    <td><?= $r["invoice_no"]; ?></td>
+                                    <td><?= $r["amount"]; ?></td>
+                                    <td><?= $r["payment_mode"]; ?></td>
+                                    <td><?= $r["ref_no"]; ?></td>
+                                    <td><?= $r["code"]; ?></td>
+                                    <td><?= $r["payment_date"]; ?></td>
+
+                                </tr><!-- tr finish -->
+
+                            <?php } } ?>
+
+                            </tbody><!-- tbody finish -->
+
+                        </table><!-- table table-hover table-striped table-bordered finish -->
+
+                    </div><!-- table-responsive finish -->
+
+                </div><!-- panel-body finish -->
+
+            </div><!-- panel panel-default finish -->
+        </div><!-- col-lg-12 finish -->
+    </div><!-- row 2 finish -->
+
+<?php } ?>
+<!-- <script>
+    function confirm_delete(){
+        if(confirm("Are you sure you want to delete this..?") === true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+</script> -->
